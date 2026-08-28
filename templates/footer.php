75b3263b3fffbbb943fc8d7fcec74af004d29d96
Koserf <izfitri36@gmail.com>
2026-08-29 02:43:19 +0800

    tuto 16 - header & footer

---
+++ templates/footer.php
@@ -0,0 +1,11 @@
+<?php
+
+// Footer
+// Displays the copyright information.
+
+?>
+
+<footer class="section">
+    <div class="container grey-text">Copyright 2019 Koserf Pizza</div>
+</footer>
+</body>
---
+++ templates/index.php
@@ -0,0 +1,16 @@
+<?php
+
+// Main page
+// Includes the header and footer templates.
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<?php
+include('templates/header.php'); ?>
+
+</body>
+
+</html>
